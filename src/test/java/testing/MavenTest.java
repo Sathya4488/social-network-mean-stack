cf714b24cb07ab5f2598505b26c14dc59e0bf308
Sathya4488 <48579570+Sathya4488@users.noreply.github.com>
2019-03-27 18:25:47 +0530

    Update MavenTest.java

--- src/test/java/testing/MavenTest.java
+++ src/test/java/testing/MavenTest.java
@@ -8,19 +8,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;	
 import org.testng.annotations.AfterTest;		
 public class MavenTest {		
-	    public WebDriver driver;		
+	    		
 		@Test				
 		public void testEasy() {	
 		System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver");
+			WebDriver driver = new FireFoxDriver();
 			driver.get("https://www.facebook.com/");  
 					
 		}	
-		@BeforeTest
-		public void beforeTest() {	
-		    driver = new FirefoxDriver();  
-		}		
-		@AfterTest
-		public void afterTest() {
-			driver.quit();			
-		}		
+			
 }	
