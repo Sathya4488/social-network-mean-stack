e3cc4443b4a6268b003442a1cb998cf959b16a92
Sathya <sathyamsb91@gmail.com>
2019-03-28 10:15:08 +0530

    added

---
+++ src/test/java/testing/MavenTest.java
@@ -0,0 +1,24 @@
+
+
+package testing;		
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class MavenTest {		
+	  //  private WebDriver driver;		
+		@Test				
+		public void testEasy() {	
+		System.setProperty("webdriver.gecko.driver", ".drivers/geckodriver");
+		WebDriver driver = new FirefoxDriver();	
+		driver.get("https://www.facebook.com/");  
+					
+		}	
+	/*
+	 * @BeforeTest public void beforeTest() { driver = new FirefoxDriver(); }
+	 * 
+	 * @AfterTest(enabled=false) public void afterTest() { driver.quit(); }
+	 */	
+}	
